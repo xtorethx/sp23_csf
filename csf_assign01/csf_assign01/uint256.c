5551a4450ab74c1a609122a5537531fef8b658f4
Dru <druzheng@gmail.com>
2023-01-26 18:03:55 -0500

    tenative wip for m1

--- csf_assign01/csf_assign01/uint256.c
+++ csf_assign01/csf_assign01/uint256.c
@@ -9,7 +9,10 @@
 // all other bits are set to 0.
 UInt256 uint256_create_from_u64(uint64_t val) {
   UInt256 result;
-  // TODO: implement
+  result.data[0] = val;
+  result.data[1] = { 0 };
+  result.data[2] = { 0 };
+  result.data[3] = { 0 };
   return result;
 }
 
@@ -18,7 +21,10 @@ UInt256 uint256_create_from_u64(uint64_t val) {
 // at index 3 is the most significant.
 UInt256 uint256_create(const uint64_t data[4]) {
   UInt256 result;
-  // TODO: implement
+  result.data[0] = data[0]};
+  result.data[1] = data[1];
+  result.data[2] = data[2];
+  result.data[3] = data[3];
   return result;
 }
 
@@ -42,7 +48,7 @@ char *uint256_format_as_hex(UInt256 val) {
 // significant 64 bits.
 uint64_t uint256_get_bits(UInt256 val, unsigned index) {
   uint64_t bits;
-  // TODO: implement
+  bits = val.data[index];
   return bits;
 }
 
