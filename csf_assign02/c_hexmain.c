011ec9bf8da6f6dc127577ef4a1b13cb6b1dc9d9
Dru <druzheng@gmail.com>
2023-02-17 18:22:18 -0500

    main

--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -3,5 +3,40 @@
 #include "hexfuncs.h"  // this is the only header file which may be included!
 
 int main(void) {
-  // TODO: implement the main function
+  unsigned counter;
+  char data_buf[16];
+  char sbuf[9];
+  char hexchar[3];
+  char colon[] = ":";
+  char space[] = " ";
+  char twospace[] = "  ";
+  char newline[] = "\n";
+  unsigned offset = 0;
+  char printable[17];
+  printable[16] = 0;
+
+  counter = hex_read(data_buf);
+  while (counter != 0) {
+    hex_format_offset(offset, sbuf);
+    hex_write_string(sbuf);
+    hex_write_string(colon);
+    hex_write_string(space);
+    for (unsigned i = 0; i < 16; i++) {
+      if (i >= counter) {
+        hex_write_string(twospace);
+        hex_write_string(space);
+        printable[i] = 32;
+      } else {
+        hex_format_byte_as_hex(data_buf[i], hexchar);
+        hex_write_string(hexchar);
+        hex_write_string(space);
+        printable[i] = hex_to_printable(data_buf[i]);
+      }
+    }
+    hex_write_string(space);
+    hex_write_string(printable);
+    hex_write_string(newline);
+    offset = offset + counter;
+    counter = hex_read(data_buf);
+  }
 }
