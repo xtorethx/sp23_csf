fd15c3458a10f1057547e201d60d2623fe0e6e37
Hannah Qu <hqu6@ugradx.cs.jhu.edu>
2023-02-27 15:45:41 -0500

    comments in main c code

--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -21,26 +21,26 @@ int main(void) {
 
   counter = hex_read(data_buf);
   while (counter != 0) {
-    hex_format_offset(offset, sbuf);
-    hex_write_string(sbuf);
-    hex_write_string(colon);
-    hex_write_string(space);
-    for (unsigned i = 0; i < 16; i++) {
-      if (i >= counter) {
-        hex_write_string(twospace);
-        hex_write_string(space);
-        printable[i] = 0;
+    hex_format_offset(offset, sbuf); //00000000
+    hex_write_string(sbuf); //write in "00000000"
+    hex_write_string(colon); //write in ":"
+    hex_write_string(space); //write in " "
+    for (unsigned i = 0; i < 16; i++) { //
+      if (i >= counter) { //after ASCII values are read in
+        hex_write_string(twospace); //write in "  "
+        hex_write_string(space); //write in " "
+        printable[i] = 0; //null terminated
       } else {
-        hex_format_byte_as_hex(data_buf[i], hexchar);
-        hex_write_string(hexchar);
-        hex_write_string(space);
-        printable[i] = hex_to_printable(data_buf[i]);
+        hex_format_byte_as_hex(data_buf[i], hexchar); //54
+        hex_write_string(hexchar); //write in 54
+        hex_write_string(space); //write in " "
+        printable[i] = hex_to_printable(data_buf[i]); //convert to T
       }
     }
-    hex_write_string(space);
-    hex_write_string(printable);
-    hex_write_string(newline);
-    offset = offset + counter;
-    counter = hex_read(data_buf);
+    hex_write_string(space); //write in " "
+    hex_write_string(printable); // write in print string
+    hex_write_string(newline); // line break
+    offset = offset + counter; //update num char read
+    counter = hex_read(data_buf); //next 16 characters
   }
 }
