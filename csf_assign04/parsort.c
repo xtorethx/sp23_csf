989082f42e58ff0fce706f286e542b95860829ac
Hannah Qu <hqu6@ugradx.cs.jhu.edu>
2023-04-07 10:17:42 -0400

    main implementation

--- csf_assign04/parsort.c
+++ csf_assign04/parsort.c
@@ -74,18 +74,51 @@ int main(int argc, char **argv) {
   const char *filename = argv[1];
   char *end;
   size_t threshold = (size_t) strtoul(argv[2], &end, 10);
-  if (end != argv[2] + strlen(argv[2]))
+  if (end != argv[2] + strlen(argv[2])) {
     /* TODO: report an error (threshold value is invalid) */;
+    fprintf(stderr, "Error: threshold value is invalid");
+    return 2;
+  }
 
   // TODO: open the file
+  int fd = open(filename, O_RDWR);
+  if (fd < 0) {
+    fprintf(stderr, "Error: file couldn't be opened");
+    return 3;
+  }
 
   // TODO: use fstat to determine the size of the file
+  struct stat statbuf;
+  int rc = fstat(fd, &statbuf);
+  if (rc != 0) {
+      fprintf(stderr, "Error: fstat error");
+      return 4;
+  }
+  size_t file_size_in_bytes = statbuf.st_size;
 
   // TODO: map the file into memory using mmap
+  int64_t *data = mmap(NULL, file_size_in_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  // you should immediately close the file descriptor here since mmap maintains a separate
+  // reference to the file and all open fds will gets duplicated to the children, which will
+  // cause fd in-use-at-exit leaks.
+  // TODO: call close()
+  close(fd);
+  if (data == MAP_FAILED) {
+      // handle mmap error and exit
+      fprintf(stderr, "Error: mmap error");
+      return 5;
+  }
+  // *data now behaves like a standard array of int64_t. Be careful though! Going off the end
+  // of the array will silently extend the file, which can rapidly lead to disk space
+  // depletion!
 
   // TODO: sort the data!
+  merge_sort(data, 0, (sizeof(data)/sizeof(data[0])), 1);
 
   // TODO: unmap and close the file
+  munmap(data, file_size_in_bytes);
+  
 
   // TODO: exit with a 0 exit code if sort was successful
+  return 0;
 }
