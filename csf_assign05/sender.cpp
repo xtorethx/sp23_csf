e1a5ac34a099b12dca1526b11e3f6abc08fa7d60
Dru <druzheng@gmail.com>
2023-04-14 17:14:53 -0400

    sender

--- csf_assign05/sender.cpp
+++ csf_assign05/sender.cpp
@@ -22,11 +22,96 @@ int main(int argc, char **argv) {
   username = argv[3];
 
   // TODO: connect to server
+  Connection sender;
+  sender.connect(server_hostname, server_port);
+  if (!sender.is_open()) {
+    std::cerr << "Connection cannot be opened!" << std::endl;
+    return 1;
+  }
 
   // TODO: send slogin message
+  Message slogin_msg;
+  slogin_msg.tag = TAG_SLOGIN;
+  slogin_msg.data = username;
+  sender.send(slogin_msg);
+
+  Message ret_msg;
+  sender.receive(ret_msg);
+  if (ret_msg.tag == TAG_ERR) {
+    std::cerr << ret_msg.data << std::endl;
+    return 1;
+  }
 
   // TODO: loop reading commands from user, sending messages to
   //       server as appropriate
+
+  std::string line;
+  std::string room_name;
+  while (std::getline(std::cin, line)) {
+    if (line.empty()) {
+      continue;
+    }
+
+    std::stringstream sin(line);
+    std::string word;
+    sin >> word;
+    Message msg;
+    if (word.compare("/join")) {
+      msg.tag = TAG_JOIN;
+
+      std::string::size_type n = 0;
+      n = line.find_first_not_of( " \t", n );
+      n = line.find_first_of( " \t", n );
+      line.erase( 0,  line.find_first_not_of( " \t", n ) );
+      room_name = line;
+      msg.data = room_name;
+      sender.send(msg);
+      
+      sender.receive(ret_msg);
+      if (ret_msg.tag == TAG_ERR) {
+        std::cerr << ret_msg.data << std::endl;
+        return 1;
+      }
+
+    } else if (word.compare("/leave")) {
+      msg.tag = TAG_LEAVE;
+      msg.data = room_name;
+      sender.send(msg);
+      
+      sender.receive(ret_msg);
+      if (ret_msg.tag == TAG_ERR) {
+        std::cerr << ret_msg.data << std::endl;
+      } 
+      
+    } else if (word.compare("/quit")) {
+      msg.tag = TAG_LEAVE;
+      msg.data = room_name;
+      sender.send(msg);
+      
+      sender.receive(ret_msg);
+      if (ret_msg.tag == TAG_ERR) {
+        std::cerr << ret_msg.data << std::endl;
+        return 1;
+      } else {
+        return 0;
+      }
+
+    } else if (word[0] == '/') {
+      std::cerr << "Invalid command" << std::endl;
+
+    } else {
+      msg.tag = TAG_SENDALL;
+      msg.data = line;
+      sender.send(msg);
+
+      sender.receive(ret_msg);
+      if (ret_msg.tag == TAG_ERR) {
+        std::cerr << ret_msg.data << std::endl;
+      }
+
+    }
+
+  }
 
   return 0;
 }
